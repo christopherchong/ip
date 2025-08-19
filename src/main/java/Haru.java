5d4e6b6f184fe8aba5890a38f49195db7e9fc284
christopherchong <chrstchong@gmail.com>
2025-08-19 22:40:50 +0800

    Rename Duke.java

---
+++ src/main/java/Haru.java
@@ -0,0 +1,9 @@
+public class Haru {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Haru\nWhat can I do for you?\n");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
