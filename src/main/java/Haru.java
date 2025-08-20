d9ecca0f573bcacc54debdc16887c2683f3931de
christopherchong <chrstchong@gmail.com>
2025-08-20 14:25:52 +0800

    Echo commands entered by user and exit on bye

--- src/main/java/Haru.java
+++ src/main/java/Haru.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 public class Haru {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Haru\nWhat can I do for you?\n");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("____________________________________________________________\n");
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________\n");
+        }
     }
 }
