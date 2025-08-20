9124c61d10bffb44eba5d7a7e362972f5c95d96c
christopherchong <chrstchong@gmail.com>
2025-08-20 14:52:48 +0800

    Store text entered by user and display when requested

--- src/main/java/Haru.java
+++ src/main/java/Haru.java
@@ -2,24 +2,40 @@ import java.util.Scanner;
 
 public class Haru {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final int MAX_TASKS = 100;
+        String[] tasks = new String[MAX_TASKS];
+        int task_count = 0;
+
+        // Introduction
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Haru\nWhat can I do for you?\n");
+        System.out.println("Hello! I'm Haru\nWhat can I do for you?");
         System.out.println("____________________________________________________________\n");
 
-        Scanner sc = new Scanner(System.in);
         while (true) {
             String input = sc.nextLine();
 
-            if (input.equals("bye")) {
+            if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                int i = 0;
+                while (tasks[i] != null) {
+                    System.out.println((i+1) + ". " + tasks[i]);
+                    i++;
+                }
+                System.out.println("____________________________________________________________\n");
+            }
+            else if (input.equals("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
             }
-
-            System.out.println("____________________________________________________________");
-            System.out.println(input);
-            System.out.println("____________________________________________________________\n");
+            else {
+                tasks[task_count++] = input;
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + input);
+                System.out.println("____________________________________________________________\n");
+            }
         }
     }
 }
