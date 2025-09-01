099c8ef9d02a47e6032ce7498c8a140e1107b6ed
christopherchong <chrstchong@gmail.com>
2025-09-01 22:39:53 +0800

    Tweak JavaFX files to comply with checkstyle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,7 @@ import javafx.stage.Stage;
  * A GUI for Haru using FXML.
  */
 public class Main extends Application {
-    Path filePath = Paths.get("src", "data", "haru.txt");
+    private final Path filePath = Paths.get("src", "data", "haru.txt");
     private final Haru haru = new Haru(filePath);
 
     @Override
@@ -23,7 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
-            fxmlLoader.<MainWindow>getController().setHaru(haru);  // inject the Haru instance
+            fxmlLoader.<MainWindow>getController().setHaru(haru);
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
