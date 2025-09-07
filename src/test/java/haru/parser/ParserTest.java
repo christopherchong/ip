836969ff5c3fb9f1515d96cb45f41168597d86d0
christopherchong <chrstchong@gmail.com>
2025-09-07 15:38:46 +0800

    Reformat test cases in ParserTest

--- src/test/java/haru/parser/ParserTest.java
+++ src/test/java/haru/parser/ParserTest.java
@@ -1,7 +1,7 @@
 package haru.parser;
 
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -21,259 +21,232 @@ public class ParserTest {
     @Test
     void parseListCommand_returnsListCommand() throws HaruException {
         Command cmd = Parser.parse("list");
-        assertTrue(cmd instanceof ListCommand);
+        assertInstanceOf(ListCommand.class, cmd);
     }
 
     // mark
     @Test
     void parseMarkCommand_returnsMarkCommand() throws HaruException {
         Command cmd = Parser.parse("mark 1");
-        assertTrue(cmd instanceof MarkCommand);
+        assertInstanceOf(MarkCommand.class, cmd);
     }
 
     @Test
     void parseMarkWithoutNumber_throwsHaruException() {
-        assertThrows(HaruException.NumberFormatException.class, () -> {
-            Parser.parse("mark");
-        });
+        assertThrows(HaruException.NumberFormatException.class, () ->
+                Parser.parse("mark"));
     }
 
     @Test
     void parseMarkWithoutNumber2_throwsHaruException() {
-        assertThrows(HaruException.NumberFormatException.class, () -> {
-            Parser.parse("mark e");
-        });
+        assertThrows(HaruException.NumberFormatException.class, () ->
+                Parser.parse("mark e"));
     }
 
     // unmark
     @Test
     void parseUnmarkCommand_returnsUnmarkCommand() throws HaruException {
         Command cmd = Parser.parse("unmark 2");
-        assertTrue(cmd instanceof UnmarkCommand);
+        assertInstanceOf(UnmarkCommand.class, cmd);
     }
 
     @Test
     void parseUnmarkWithoutNumber_throwsHaruException() {
-        assertThrows(HaruException.NumberFormatException.class, () -> {
-            Parser.parse("unmark");
-        });
+        assertThrows(HaruException.NumberFormatException.class, () ->
+                Parser.parse("unmark"));
     }
 
     @Test
     void parseUnmarkWithoutNumber2_throwsHaruException() {
-        assertThrows(HaruException.NumberFormatException.class, () -> {
-            Parser.parse("unmark e");
-        });
+        assertThrows(HaruException.NumberFormatException.class, () ->
+                Parser.parse("unmark e"));
     }
 
     // to-do
     @Test
     void parseTodoCommand_returnsAddCommand() throws HaruException {
         Command cmd = Parser.parse("todo read book");
-        assertTrue(cmd instanceof AddCommand);
+        assertInstanceOf(AddCommand.class, cmd);
     }
 
     @Test
     void parseTodoWithoutDescription_throwsHaruException() {
-        assertThrows(HaruException.InvalidTodoException.class, () -> {
-            Parser.parse("todo");
-        });
+        assertThrows(HaruException.InvalidTodoException.class, () ->
+                Parser.parse("todo"));
     }
 
     // deadline
     @Test
     void parseDeadlineCommand_returnsAddCommand() throws HaruException {
         Command cmd = Parser.parse("deadline return book /by 2/12/2019 1800");
-        assertTrue(cmd instanceof AddCommand);
+        assertInstanceOf(AddCommand.class, cmd);
     }
 
     @Test
     void parseDeadlineWithoutBy_throwsHaruException() {
-        assertThrows(HaruException.InvalidDeadlineException.class, () -> {
-            Parser.parse("deadline return book 2/12/2019 1800");
-        });
+        assertThrows(HaruException.InvalidDeadlineException.class, () ->
+                Parser.parse("deadline return book 2/12/2019 1800"));
     }
 
     @Test
     void parseDeadlineWithInvalidDate_throwsHaruException() {
-        assertThrows(HaruException.DateTimeParseException.class, () -> {
-            Parser.parse("deadline return book /by 2/15/2019 1800");
-        });
+        assertThrows(HaruException.DateTimeParseException.class, () ->
+                Parser.parse("deadline return book /by 2/15/2019 1800"));
     }
 
     // event
     @Test
     void parseEventCommand_returnsAddCommand() throws HaruException {
         Command cmd = Parser.parse("event project meeting /from 2/12/2019 1400 /to 2/12/2019 1600");
-        assertTrue(cmd instanceof AddCommand);
+        assertInstanceOf(AddCommand.class, cmd);
     }
 
     @Test
     void parseEventWithoutFrom_throwsHaruException() {
-        assertThrows(HaruException.InvalidEventException.class, () -> {
-            Parser.parse("event meeting 2/12/2019 1600 /to 2/12/2019 1400");
-        });
+        assertThrows(HaruException.InvalidEventException.class, () ->
+                Parser.parse("event meeting 2/12/2019 1600 /to 2/12/2019 1400"));
     }
 
     @Test
     void parseEventWithoutTo_throwsHaruException() {
-        assertThrows(HaruException.InvalidEventException.class, () -> {
-            Parser.parse("event meeting /from 2/12/2019 1600 2/12/2019 1700");
-        });
+        assertThrows(HaruException.InvalidEventException.class, () ->
+                Parser.parse("event meeting /from 2/12/2019 1600 2/12/2019 1700"));
     }
 
     @Test
     void parseEventWithInvalidFrom_throwsHaruException() {
-        assertThrows(HaruException.DateTimeParseException.class, () -> {
-            Parser.parse("event meeting /from 2/15/2019 1600 /to 2/12/2019 1400");
-        });
+        assertThrows(HaruException.DateTimeParseException.class, () ->
+                Parser.parse("event meeting /from 2/15/2019 1600 /to 2/12/2019 1400"));
     }
 
     @Test
     void parseEventWithInvalidTo_throwsHaruException() {
-        assertThrows(HaruException.DateTimeParseException.class, () -> {
-            Parser.parse("event meeting /from 2/12/2019 1600 /to 2/15/2019 1400");
-        });
+        assertThrows(HaruException.DateTimeParseException.class, () ->
+                Parser.parse("event meeting /from 2/12/2019 1600 /to 2/15/2019 1400"));
     }
 
     @Test
     void parseEventWithSameDates_throwsHaruException() {
-        assertThrows(HaruException.SameDateTimeException.class, () -> {
-            Parser.parse("event meeting /from 2/12/2019 1600 /to 2/12/2019 1600");
-        });
+        assertThrows(HaruException.SameDateTimeException.class, () ->
+                Parser.parse("event meeting /from 2/12/2019 1600 /to 2/12/2019 1600"));
     }
 
     @Test
     void parseEventWithInvalidDateOrder_throwsHaruException() {
-        assertThrows(HaruException.DateTimeOrderException.class, () -> {
-            Parser.parse("event meeting /from 2/12/2019 1600 /to 2/12/2019 1400");
-        });
+        assertThrows(HaruException.DateTimeOrderException.class, () ->
+                Parser.parse("event meeting /from 2/12/2019 1600 /to 2/12/2019 1400"));
     }
 
     // delete
     @Test
     void parseDeleteCommand_returnsDeleteCommand() throws HaruException {
         Command cmd = Parser.parse("delete 3");
-        assertTrue(cmd instanceof DeleteCommand);
+        assertInstanceOf(DeleteCommand.class, cmd);
     }
 
     @Test
     void parseDeleteWithoutNumber_throwsHaruException() {
-        assertThrows(HaruException.NumberFormatException.class, () -> {
-            Parser.parse("delete");
-        });
+        assertThrows(HaruException.NumberFormatException.class, () ->
+                Parser.parse("delete"));
     }
 
     @Test
     void parseDeleteWithoutNumber2_throwsHaruException() {
-        assertThrows(HaruException.NumberFormatException.class, () -> {
-            Parser.parse("delete e");
-        });
+        assertThrows(HaruException.NumberFormatException.class, () ->
+                Parser.parse("delete e"));
     }
 
     // find
     @Test
     void parseFindCommand_returnsFindCommand() throws HaruException {
         Command cmd = Parser.parse("find book");
-        assertTrue(cmd instanceof FindCommand);
+        assertInstanceOf(FindCommand.class, cmd);
     }
 
     @Test
     void parseFindWithoutKeyword_throwsHaruException() {
-        assertThrows(HaruException.InvalidFindException.class, () -> {
-            Parser.parse("find");
-        });
+        assertThrows(HaruException.InvalidFindException.class, () ->
+                Parser.parse("find"));
     }
 
     // tag
     @Test
     void parseTagCommand_returnsTagCommand() throws HaruException {
         Command cmd = Parser.parse("tag 1 #test");
-        assertTrue(cmd instanceof TagCommand);
+        assertInstanceOf(TagCommand.class, cmd);
     }
 
     @Test
     void parseTagWithNoArguments_throwsHaruException() {
-        assertThrows(HaruException.NoTagException.class, () -> {
-            Parser.parse("tag");
-        });
+        assertThrows(HaruException.NoTagException.class, () ->
+                Parser.parse("tag"));
     }
 
     @Test
     void parseTagWithNoIndex_throwsHaruException() {
-        assertThrows(HaruException.NoTagException.class, () -> {
-            Parser.parse("tag #test");
-        });
+        assertThrows(HaruException.NoTagException.class, () ->
+                Parser.parse("tag #test"));
     }
 
     @Test
     void parseTagWithInvalidIndex_throwsHaruException() {
-        assertThrows(HaruException.InvalidTagFormatException.class, () -> {
-            Parser.parse("tag e #test");
-        });
+        assertThrows(HaruException.InvalidTagFormatException.class, () ->
+                Parser.parse("tag e #test"));
     }
 
     @Test
     void parseTagWithNoTag_throwsHaruException() {
-        assertThrows(HaruException.NoTagException.class, () -> {
-            Parser.parse("tag 1");
-        });
+        assertThrows(HaruException.NoTagException.class, () ->
+                Parser.parse("tag 1"));
     }
 
     @Test
     void parseTagWithInvalidTag_throwsHaruException() {
-        assertThrows(HaruException.InvalidTagFormatException.class, () -> {
-            Parser.parse("tag 1 test");
-        });
+        assertThrows(HaruException.InvalidTagFormatException.class, () ->
+                Parser.parse("tag 1 test"));
     }
 
     // untag
     @Test
     void parseUntagCommand_returnsUntagCommand() throws HaruException {
         Command cmd = Parser.parse("untag 1 #test");
-        assertTrue(cmd instanceof UntagCommand);
+        assertInstanceOf(UntagCommand.class, cmd);
     }
 
     @Test
     void parseUntagWithNoArguments_throwsHaruException() {
-        assertThrows(HaruException.NoTagException.class, () -> {
-            Parser.parse("untag");
-        });
+        assertThrows(HaruException.NoTagException.class, () ->
+                Parser.parse("untag"));
     }
 
     @Test
     void parseUntagWithNoIndex_throwsHaruException() {
-        assertThrows(HaruException.NoTagException.class, () -> {
-            Parser.parse("untag #test");
-        });
+        assertThrows(HaruException.NoTagException.class, () ->
+                Parser.parse("untag #test"));
     }
 
     @Test
     void parseUntagWithInvalidIndex_throwsHaruException() {
-        assertThrows(HaruException.InvalidTagFormatException.class, () -> {
-            Parser.parse("untag e #test");
-        });
+        assertThrows(HaruException.InvalidTagFormatException.class, () ->
+                Parser.parse("untag e #test"));
     }
 
     @Test
     void parseUntagWithNoTag_throwsHaruException() {
-        assertThrows(HaruException.NoTagException.class, () -> {
-            Parser.parse("untag 1");
-        });
+        assertThrows(HaruException.NoTagException.class, () ->
+                Parser.parse("untag 1"));
     }
 
     @Test
     void parseUntagWithInvalidTag_throwsHaruException() {
-        assertThrows(HaruException.InvalidTagFormatException.class, () -> {
-            Parser.parse("untag 1 test");
-        });
+        assertThrows(HaruException.InvalidTagFormatException.class, () ->
+                Parser.parse("untag 1 test"));
     }
 
     // invalid command
     @Test
     void parseInvalidCommand_throwsHaruException() {
-        assertThrows(HaruException.InvalidCommandException.class, () -> {
-            Parser.parse("blah");
-        });
+        assertThrows(HaruException.InvalidCommandException.class, () ->
+                Parser.parse("blah"));
     }
 }
